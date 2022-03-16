74b872b0c51e9cc28e6f8ce2bd408fb4543fd3e9
Kennedy Barros <kennedybarros413@gmail.com>
2022-03-16 15:03:55 -0400

    add FAQS

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,32 @@ function linkAction() {
   navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/*=============== QUESTIONS ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.faqs_item');
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.faqs_header');
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open');
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.faqs_content');
+
+  if (item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style');
+    item.classList.remove('accordion-open');
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion-open');
+  }
+};
